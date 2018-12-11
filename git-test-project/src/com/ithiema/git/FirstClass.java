ce60e680584be1855c722f2da91ad0aed6006b47
958401933@qq.com <Zj1303643037>
2018-12-11 17:42:29 +0800

    本地工程

---
+++ git-test-project/src/com/ithiema/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.ithiema.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
